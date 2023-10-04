18dc48211b83627d72d5a554c885fa2afee00de4
MarianaDam420 <utm23090799@utma.edu.mx>
2023-10-03 20:06:54 -0600

    Added todo components

---
+++ src/components/ListItem.jsx
@@ -0,0 +1,26 @@
+import { useState } from "react";
+
+function ListItem (text, checked, onDelete){
+    const [checked, setChecked] = useState(false);
+
+    const handleCheck = () => {
+
+        setChecked(!checked);
+        
+    }
+
+return (
+
+<li className={ `bg-indigo-800 p-2  ${checked ? '{'line-through' : null}`}>
+    {text}<input type="checkbox"checked={checked} onChange={handleCheck}/>
+    
+     </li>
+     
+     
+     
+     )
+
+}
+
+
+export default ListItem;
